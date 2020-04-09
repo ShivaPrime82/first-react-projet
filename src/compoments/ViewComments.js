cc95cb32111e6eba803db318dddee5d487d080be
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-04-09 14:48:12 +0200

    move display comments to ViewComments and create him compoments

---
+++ src/compoments/ViewComments.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+import { formatDate } from '../utils/date';
+import { toast } from 'react-toastify';
+import Card from 'react-bootstrap/Card';
+
+const ViewComments = ({ match }) => {
+    const { id } = match.params;
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/comments?id=' + id)
+            .then((result) => {
+                return result.json();
+            })
+            .then(({ status, comments }) => {
+                if (status === "OK") {
+                    setComments(comments);
+                } else {
+                    toast.error("Oups ... Nous avons eu une erreur !");
+                }
+            })
+            .catch((error) => {
+                toast.error("Oups ... Nous avons eu une erreur !");
+                console.log(error);
+            })
+    }, [id])
+
+    const renderedComments = comments.map((comment) => {
+        const { id, content, created_at, authorFirstname, authorLastname } = comment;
+        return (
+            <Card key={id}>
+                <Card.Header>
+                    <small className="text-muted">
+                        créé le&nbsp;
+                        {formatDate(created_at)}&nbsp;
+                        par&nbsp;{authorFirstname}&nbsp;{authorLastname}.
+                    </small>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        {content}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    });
+
+    return renderedComments;
+}
+
+export default ViewComments;
--- src/pages/ViewArticle.js
+++ src/pages/ViewArticle.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import { formatDate } from '../utils/date';
 
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import { toast } from 'react-toastify';
+
+import ViewComments from '../compoments/ViewComments';
 
 const ViewArticle = ({ match }) => {
     const { id } = match.params;
     const [article, setArticle] = useState({});
-    const [comments, setComments] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3001/api/article?id=' + id)
@@ -29,44 +29,6 @@ const ViewArticle = ({ match }) => {
             })
     }, [id])
 
-    useEffect(() => {
-        fetch('http://localhost:3001/api/comments?id=' + id)
-            .then((result) => {
-                return result.json();
-            })
-            .then(({ status, comments }) => {
-                if (status === "OK") {
-                    setComments(comments);
-                } else {
-                    toast.error("Oups ... Nous avons eu une erreur !");
-                }
-            })
-            .catch((error) => {
-                toast.error("Oups ... Nous avons eu une erreur !");
-                console.log(error);
-            })
-    }, [id])
-
-    const renderedComments = comments.map((comment) => {
-        const { id, content, created_at, authorFirstname, authorLastname } = comment;
-        return (
-            <Card key={id}>
-                <Card.Header>
-                    <small className="text-muted">
-                        créé le&nbsp;
-                        {formatDate(created_at)}&nbsp;
-                        par&nbsp;{authorFirstname}&nbsp;{authorLastname}.
-                    </small>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        {content}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        );
-    });
-
     return (
         <Container>
             <h1>{article.title}</h1>
@@ -78,7 +40,7 @@ const ViewArticle = ({ match }) => {
                 par {article.authorFirstname} {article.authorLastname}
             </p>
             <div>
-                {renderedComments}
+                <ViewComments />
             </div>
         </Container>
     );
